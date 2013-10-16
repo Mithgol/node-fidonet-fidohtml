67019e8f3520c3ded86baed46c6fbf2bca997c4c
Mithgol the Webmaster <getgit@mithgol.ru>
2013-10-16 23:22:40 +0400

    convert some URLs to hyperlinks

--- fidohtml.js
+++ fidohtml.js
@@ -2,10 +2,24 @@ var FidoHTML = function(){
    if (!(this instanceof FidoHTML)) return new FidoHTML();
 };
 
-FidoHTML.prototype.fromText = function(msgText){
-   var out = msgText.replace(/\r?\n/g, '<br>');
+var afterURL = function(middle){
+   var out = middle.replace(/\r?\n/g, '<br>');
    out = out.replace(/  /g, ' \u00A0');
    return out;
+};
+
+FidoHTML.prototype.fromText = function(msgText){
+   /* jshint -W101 */
+   var arrHyper = msgText.split(
+      /(\b(?:https?|ftp|mailto|aim|bitcoin|ed2k|facetime|feed|geo|irc(?:6|s)?|magnet|news|nntp|sips?|skype|sms|ssh|tel|telnet|tftp|xmpp):.*?)(?=$|[\s<>\x22\x27{}|\^\[\]`])/
+   );
+   for( var i = 0; i < arrHyper.length; i+=2 ){
+      arrHyper[i] = afterURL( arrHyper[i] );
+   }
+   for( var j = 1; j < arrHyper.length; j+=2 ){
+      arrHyper[j] = '<a href="' + arrHyper[j] + '">' + arrHyper[j] + '</a>';
+   }
+   return arrHyper.join('');
 };
 
 module.exports = FidoHTML;
